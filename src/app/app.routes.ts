8bdd4a2e94ef74ca78ecb0f78a85dd2337ba35e1
JuanDiPosada <juandiegocifuentesposada@gmail.com>
2026-02-07 13:29:26 -0500

    feat: Implement initial application structure with header, info page, and pomodoro timer UI.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,15 @@ import { HabitsPage } from './modules/habits/pages/habits-page/habits-page';
 import { GamesPage } from './modules/games/pages/games-page/games-page';
 
 export const routes: Routes = [
-    { path: 'pomodoro', component: PomodoroPage},
-    { path: 'habits', component: HabitsPage},
-    { path: 'games', component: GamesPage},
-    { path: '', redirectTo: 'pomodoro', pathMatch: 'full' },
+  { path: 'pomodoro', component: PomodoroPage },
+  { path: 'habits', component: HabitsPage },
+  { path: 'games', component: GamesPage },
+  {
+    path: 'info',
+    loadComponent: () =>
+      import('./modules/info/pages/info-page/info-page').then(
+        (m) => m.InfoPage,
+      ),
+  },
+  { path: '', redirectTo: 'info', pathMatch: 'full' },
 ];
---
+++ src/app/modules/info/pages/info-page/info-page.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-info-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './info-page.html',
+  styleUrl: './info-page.css',
+})
+export class InfoPage implements OnInit {
+  deferredPrompt: any;
+  showInstallButton = false;
+
+  ngOnInit() {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      this.deferredPrompt = e;
+      // Update UI notify the user they can install the PWA
+      this.showInstallButton = true;
+    });
+
+    window.addEventListener('appinstalled', () => {
+      // Hide the app-provided install promotion
+      this.showInstallButton = false;
+      this.deferredPrompt = null;
+    });
+  }
+
+  async installPwa() {
+    if (!this.deferredPrompt) {
+      return;
+    }
+    // Show the install prompt
+    this.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    const { outcome } = await this.deferredPrompt.userChoice;
+    // We've used the prompt, and can't use it again, throw it away
+    this.deferredPrompt = null;
+    this.showInstallButton = false;
+  }
+}
